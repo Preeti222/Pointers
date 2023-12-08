dcb9fc821f61b8d4daecd8d58d69a6ed041c1c0b
Preeti222 <preeti22@navgurukul.org>
2023-12-08 10:48:16 +0530

    pointer

---
+++ typecasting.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+// int main(){
+//     int a=137;
+//     int *p=&a;
+//     char *po;
+//     po=(char*)p;
+//     printf("%p \n",po);
+//     printf("%d \n",*po);
+//     printf("%d \n",*(po+1));
+//     return 0;
+// }
+
+#include<stdio.h>
+int main(){
+    int x=2023;
+    int x=;
+    printf("hello navgurukul \n :%c",x);
+    return 0;
+}
